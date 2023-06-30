f8bc8f848e6f64e5f2717f084fc5e591fdce25d2
Nedzik <113420019+Nedzik97@users.noreply.github.com>
2023-06-30 10:35:59 +0300

    created the main page

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import { Main } from '../main/main';
+
+export type AppScreenProps = {
+  filmTitle: string;
+  genre: string;
+  movieReleaseDate: number;
 }
 
+const App = ({filmTitle, genre, movieReleaseDate}: AppScreenProps): JSX.Element =>
+  <Main filmTitle={filmTitle} genre={genre} movieReleaseDate={movieReleaseDate}/>;
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app/app';
+import App, { AppScreenProps } from './components/app/app';
+
+
+const filmInfo: AppScreenProps = {
+  filmTitle: 'Титаник',
+  genre: 'мелодрама',
+  movieReleaseDate: 2002
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -8,6 +15,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App filmInfo={filmInfo}/>
   </React.StrictMode>,
 );
